d30376fbcc00a8a7d48012b8d58b6c25377cd440
Vic <250506628>
2017-08-21 23:52:10 +0800

    1、新增htmlparse依赖； 2、新增调用百度查询身份证信息接口； 3、新增实体类IdNoInfo。

--- src/main/java/com/test/httpclient/HttpClientUtils.java
+++ src/main/java/com/test/httpclient/HttpClientUtils.java
@@ -8,6 +8,11 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.htmlparser.Node;
+import org.htmlparser.Parser;
+import org.htmlparser.filters.TagNameFilter;
+import org.htmlparser.util.NodeList;
+import org.htmlparser.util.ParserException;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -47,8 +52,8 @@ public class HttpClientUtils {
 		if (ip == null || ip.trim().length() == 0) {
 			throw new IllegalArgumentException("参数错误!");
 		}
-		String url = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?co=&resource_id=6006&t=1501510188699&ie=utf8&oe=gbk&format=json&query=" + ip;
-		String result = get(url);
+		String url = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?co=&resource_id=6006&t=1501510188699&ie=utf8&oe=gbk&format=json&query=";
+		String result = get(url + ip);
 		JSONObject jsonObject = JSON.parseObject(result);
 		if (jsonObject.getJSONArray("data").size() == 0) {
 			System.out.println("ip地址未查询到信息 !");
@@ -60,8 +65,43 @@ public class HttpClientUtils {
 		jsonObject = jsonObject.getJSONArray("data").getJSONObject(0);
 		System.out.println(jsonObject.getString("origip") + "  " + jsonObject.getString("location"));
 	}
+	
+	public static void queryIdNoInfo(String idNo) throws Exception {
+		String url = "https://sp0.baidu.com/5aU_bSa9KgQFm2e88IuM_a/sfzcxgg.duapp.com/index.php";
+		String html = post(url, "in_id="+ idNo + "&submit=查 询");
+		Parser parser = Parser.createParser(html, "UTF-8");
+		IdNoInfo idNoInfo = handleQueryIdInfoResult(parser);
+		System.out.println(idNoInfo);
+	}
+
+	private static IdNoInfo handleQueryIdInfoResult(Parser parser) throws ParserException {
+		TagNameFilter tagNameFilter = new TagNameFilter("div");
+		NodeList nodeList = parser.extractAllNodesThatMatch(tagNameFilter);
+		
+		Node node = null;
+		IdNoInfo idNoInfo = new IdNoInfo();
+		for (int i = 0; i <= nodeList.size(); i++) {
+			node = nodeList.elementAt(i);
+			if (node == null || node.getText() == null) {
+				continue;
+			}
+			if (node.toPlainTextString().contains("18位身份证号")) {
+				idNoInfo.setIdNo(nodeList.elementAt(i + 1).toPlainTextString());
+			}
+			if (node.toPlainTextString().contains("发证地")) {
+				idNoInfo.setAddr(nodeList.elementAt(i + 1).toPlainTextString());
+			}
+			if (node.toPlainTextString().contains("出生日期")) {
+				idNoInfo.setBrithday(nodeList.elementAt(i + 1).toPlainTextString());
+			}
+			if (node.toPlainTextString().contains("性别")) {
+				idNoInfo.setSex(nodeList.elementAt(i + 1).toPlainTextString());
+			}
+		}
+		return idNoInfo;
+	}
 
 	public static void main(String[] args) throws Exception {
-		queryIPAddrInfo("116.224.229.50");
+		queryIdNoInfo("360281199110215216");
 	}
 }
