745ea830f2d002ba0458c69a33e29ab7bbb55e53
wendy <tester_lo@163.com>
2018-03-14 14:44:36 +0800

    2

---
+++ src/main/java/com/test/array/ArrayListUse.java
@@ -0,0 +1,15 @@
+package com.test.array;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ArrayListUse {
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		Collection c1 = new ArrayList();
+		c1.add(1);
+		c1.add("KaoPu");
+	}
+
+}
