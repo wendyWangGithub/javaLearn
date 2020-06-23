88e5b54b593e00479a7e2d8becc3afadba26c4be
wendy <tester_lo@163.com>
2020-06-23 14:47:42 +0800

    Wwr发送httpget请求

---
+++ src/main/java/com/test/httpclient/WwrHttp.java
@@ -0,0 +1,12 @@
+package main.java.com.test.httpclient;
+
+public class WwrHttp {
+
+    public static void main(String [] args) {
+        /**
+         * 调用httpget请求
+         */
+        WwrHttpRequestTest hrt = new WwrHttpRequestTest();
+        hrt.doGet("http://www.baidu.com");
+    }
+}
